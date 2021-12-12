d24d8eef3cb8a00c7d9e5e5092d5010e6baee9ff
Kovach-42 <mortonmu8@gmail.com>
2021-12-12 19:06:50 +0200

    first commit

---
+++ pages/events/[...slug].js
@@ -0,0 +1,72 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import { getFilteredEvents } from "../../dummy-data";
+import ErrorAlert from "../../components/ui/error-alert";
+
+function FilteredEvents() {
+  const router = useRouter();
+  const filteredData = router.query.slug;
+
+  // if data is undefined
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+
+  // a + at the end of filteredYear and filteredMonth converts them into integers
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid Filter. Please adjust your values</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No Events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show all Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+}
+
+export default FilteredEvents;
